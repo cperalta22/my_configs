c148d78474054e1d72fb699e83136bd451b247ff
Carlos Alberto Peralta Alvarez <drperaltaxp@hotmail.com>
2023-05-06 08:51:05 -0600

    actualizacion_monitores

--- monitor_setting.sh
+++ monitor_setting.sh
@@ -2,7 +2,13 @@
 #
 
 hdmi=$(xrandr | grep "HDMI-1 connected" | wc -l)
+usbc=$(xrandr | grep -E "^DP-1 connected" | wc -l)
+
 
 if [ $hdmi -eq 1 ]; then
-  /home/cpa/.screenlayout/casa.sh
+  if [ $usbc -eq 1 ]; then
+    /home/cpa/.screenlayout/casa3mon.sh && nitrogen --restore
+  else
+    /home/cpa/.screenlayout/casa.sh && nitrogen --restore
+  fi
 fi
