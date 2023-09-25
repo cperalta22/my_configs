0a852c2e01c0a6b98da50609dd0f0cbc1eaf9e0f
Carlos Peralta <drperaltaxp@hotmail.com>
2023-09-25 06:08:13 -0600

    quito_sleep_del_script

--- tailscale_dns_fix.sh
+++ tailscale_dns_fix.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
-/usr/bin/sleep 40
 /usr/bin/ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
 /usr/bin/systemctl restart systemd-resolved
 /usr/bin/systemctl restart NetworkManager
