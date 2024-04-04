1ed7f3a100c46068cc4449f2f28f7f4b4a948a33
carlosStardust <cperalta@ifc.unam.mx>
2024-04-04 13:41:38 -0600

    Agregada lista de software básico

---
+++ softwareForNewMachineManjaro.sh
@@ -0,0 +1,3 @@
+sudo pacman -Syu
+sudo pacman -S yay tldr exa bat neovim btop nvtop gum fd tree github-cli fzf neofetch ttf-mononoki-nerd rofi s-tui tailscale
+yay -S rofi-themes-collection-git google-chrome geekbench
