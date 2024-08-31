d054e25f35e72fd01a34acce7d07bc304a41c736
Carlos Peralta <peraltaholmes@gmail.com>
2024-08-31 16:14:30 -0600

    Cambiando pulse por pipewire

--- c3wm_manjaroi3/software_c3wm_manjaroi3.sh
+++ c3wm_manjaroi3/software_c3wm_manjaroi3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yay -S google-chrome eza bat rofi fastfetch alacritty flameshot xfce4-appfinder fd okular inkscape base-devel pacseek ttf-mononoki-nerd neovim otf-font-awesome nvtop s-tui stress tldr pasystray manjaro-pulse pavucontrol github-cli
+yay -S base-devel google-chrome eza bat rofi fastfetch alacritty flameshot xfce4-appfinder fd okular inkscape  pacseek ttf-mononoki-nerd neovim otf-font-awesome nvtop s-tui stress tldr pasystray manjaro-pipewire pavucontrol github-cli
