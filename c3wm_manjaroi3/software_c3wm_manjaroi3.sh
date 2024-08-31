178346bc2d1ef6d961bda4ce99d1a3e04c847cf1
Carlos Peralta <peraltaholmes@gmail.com>
2024-08-31 15:59:27 -0600

    Modificado para incluir el software mas elemental en manjaro, tambien cambia se cambia el audio a pulse audio a través de instalar las dependencias necesarias y cambiar el archivo de configuracion que se está utilizando

---
+++ c3wm_manjaroi3/software_c3wm_manjaroi3.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+yay -S google-chrome eza bat rofi fastfetch alacritty flameshot xfce4-appfinder fd okular inkscape base-devel pacseek ttf-mononoki-nerd neovim otf-font-awesome nvtop s-tui stress tldr pasystray manjaro-pulse pavucontrol github-cli
