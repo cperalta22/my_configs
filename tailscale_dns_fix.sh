ea8642d350169ab3830d64d595a8aea24e2f69fe
Carlos Peralta <peraltaholmes@gmail.com>
2024-10-01 06:40:20 -0600

    bbq crash

--- tailscale_dns_fix.sh
+++ tailscale_dns_fix.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-/usr/bin/ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+#/usr/bin/ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
 /usr/bin/systemctl restart systemd-resolved
 /usr/bin/systemctl restart NetworkManager
 /usr/bin/systemctl restart tailscaled.service
