6837d679f52902b76a8651893c7abc7b74b19aa4
cpa@bbq <cperalta@ifc.unam.mx>
2024-10-02 19:15:28 -0600

    primeros fixes endeavouri3

---
+++ c3wm_manjaroi3/software_c3wm_endeavouri3.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+yay -S base-devel google-chrome eza bat rofi fastfetch alacritty flameshot xfce4-appfinder fd okular inkscape pacseek ttf-mononoki-nerd neovim otf-font-awesome nvtop s-tui stress tldr pasystray github-cli gum zsh-autosuggestions pa-applet openrgb  byobu blueberry vim rpi-imager
