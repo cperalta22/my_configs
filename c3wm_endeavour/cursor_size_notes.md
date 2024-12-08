cc6fbb454894d645708a031ae5324a7a26254e5c
cpa@bbq <cperalta@ifc.unam.mx>
2024-12-08 09:08:38 -0600

    instrucciones cambio de cursor

---
+++ c3wm_endeavour/cursor_size_notes.md
@@ -0,0 +1,15 @@
+# Tamaño del cursor de mouse en i3 / mouse cursor size on i3
+
+hay que hacer o modificar ~/.Xresources
+
+un ejemplo es el siguente: 
+
+```Bash
+Xcursor.theme: Qogir
+Xcursor.size: 20
+```
+
+Pero también hay que modificar ~/.config/gtk-3.0/settings.ini en el parámetro que contenga referencia al tamaño del cursor.
+
+```
+```
--- c3wm_endeavour/software_c3wm_endeavouri3.sh
+++ c3wm_endeavour/software_c3wm_endeavouri3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yay -S picom nitrogen base-devel google-chrome eza bat rofi fastfetch alacritty flameshot xfce4-appfinder fd okular inkscape pacseek ttf-mononoki-nerd neovim otf-font-awesome nvtop s-tui stress tldr pasystray github-cli gum zsh-autosuggestions pa-applet openrgb byobu blueberry vim rpi-imager flatpak parcellite easyeffects easyeffects-bundy01-presets calf lxappearance
+yay -S picom nitrogen base-devel google-chrome eza bat rofi fastfetch alacritty flameshot xfce4-appfinder fd okular inkscape pacseek ttf-mononoki-nerd neovim otf-font-awesome nvtop s-tui stress tldr pasystray github-cli gum zsh-autosuggestions pa-applet openrgb byobu blueberry vim rpi-imager flatpak parcellite easyeffects easyeffects-bundy01-presets calf lxappearance btop zotero-bin
