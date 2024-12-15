85f18bc13980801b1ed4f87b1e88cc8eed7cda13
cpa@bbq <cperalta@ifc.unam.mx>
2024-12-15 10:03:37 -0600

    script de respaldo comenta lineas adhoc y respalda archivo zshrc

--- c3wm_endeavour/bk_configs_to_repo_endeavour.sh
+++ c3wm_endeavour/bk_configs_to_repo_endeavour.sh
@@ -2,5 +2,8 @@
 
 cp /home/$USER/.alacritty.toml ./alacritty.toml
 cp /home/$USER/.config/i3/config ./i3config
+sed -i '/screenlayout/s/^/#/' ./i3config
+sed -i '/openrgb/s/^/#/' ./i3config
 cp /etc/i3status.conf ./i3status.conf
 cp /home/$USER/.config/rofi/rofidmenu.rasi ./rofidmenu.rasi
+cp /home/$USER/.zshrc ./zshrc-$HOSTNAME
