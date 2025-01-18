13de6c8dc17591271e15f6b06a599ada69cc8c57
cpa@bbq <cperalta@ifc.unam.mx>
2025-01-18 08:33:53 -0600

    agrego fix de wacom para bbq

---
+++ fixwacombbq.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+xsetwacom set "Wacom One by Wacom M Pen stylus" rotate half
+xsetwacom set "Wacom One by Wacom M Pen eraser" rotate half
+xsetwacom set "Wacom One by Wacom M Pen stylus" maptooutput head-1
+xsetwacom set "Wacom One by Wacom M Pen eraser" maptooutput head-1
