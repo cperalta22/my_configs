0d96a098f73b66bf24640ba9342ac3f67140a434
cpa minitron <cpa@minitron>
2025-02-22 19:49:06 -0600

    fix de c3wm en endeavour, config mejorado, cambio a i3blocks

--- c3wm_endeavour/bk_configs_to_repo_endeavour.sh
+++ c3wm_endeavour/bk_configs_to_repo_endeavour.sh
@@ -2,8 +2,8 @@
 
 cp /home/$USER/.alacritty.toml ./alacritty.toml
 cp /home/$USER/.config/i3/config ./i3config
+cp /home/$USER/.config/i3/i3blocks.conf ./i3blocks
 sed -i '/screenlayout/s/^/#/' ./i3config
 sed -i '/openrgb/s/^/#/' ./i3config
 cp /etc/i3status.conf ./i3status.conf
 cp /home/$USER/.config/rofi/rofidmenu.rasi ./rofidmenu.rasi
-cp /home/$USER/.zshrc ./zshrc-$HOSTNAME
--- c3wm_endeavour/software_c3wm_endeavouri3.sh
+++ c3wm_endeavour/software_c3wm_endeavouri3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yay -S picom xclip nitrogen base-devel google-chrome eza bat rofi fastfetch alacritty flameshot xfce4-appfinder fd okular inkscape pacseek ttf-mononoki-nerd neovim otf-font-awesome nvtop s-tui stress tldr pasystray github-cli gum zsh-autosuggestions pa-applet-git openrgb byobu blueberry vim rpi-imager flatpak copyq easyeffects easyeffects-bundy01-presets calf lxappearance btop zotero-bin vivaldi vivaldi-ffmpeg-codecs tailscale tailscale-systray-git ranger filebrowser-bin
+yay -S picom xclip nitrogen base-devel google-chrome eza bat rofi fastfetch alacritty flameshot xfce4-appfinder fd okular inkscape pacseek ttf-mononoki-nerd neovim otf-font-awesome nvtop s-tui stress tldr pasystray github-cli gum zsh-autosuggestions pa-applet-git openrgb byobu blueberry vim rpi-imager flatpak copyq easyeffects easyeffects-bundy01-presets calf lxappearance btop zotero-bin ranger filebrowser-bin
