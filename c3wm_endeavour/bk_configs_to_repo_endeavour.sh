c75b312477f0fdd2f95cf2b016a8e04ee53eeee0
Carloslichu <cperalta@ifc.unam.mx>
2025-02-23 20:25:43 -0600

    fixes c3wm_endeavouros

--- c3wm_endeavour/bk_configs_to_repo_endeavour.sh
+++ c3wm_endeavour/bk_configs_to_repo_endeavour.sh
@@ -2,8 +2,9 @@
 
 cp /home/$USER/.alacritty.toml ./alacritty.toml
 cp /home/$USER/.config/i3/config ./i3config
-cp /home/$USER/.config/i3/i3blocks.conf ./i3blocks
+cp /home/$USER/.config/i3/i3blocks.conf ./i3blocks.conf
 sed -i '/screenlayout/s/^/#/' ./i3config
 sed -i '/openrgb/s/^/#/' ./i3config
 cp /etc/i3status.conf ./i3status.conf
 cp /home/$USER/.config/rofi/rofidmenu.rasi ./rofidmenu.rasi
+cp /home/$USER/.config/rofi/config.rasi ./config.rasi
--- c3wm_endeavour/cp_configs_to_final_location_endeavouri3.sh
+++ c3wm_endeavour/cp_configs_to_final_location_endeavouri3.sh
@@ -3,5 +3,7 @@
 cp ./alacritty.toml /home/$USER/.alacritty.toml
 [ ! -d /home/$USER/.config/rofi ] && mkdir -p /home/$USER/.config/rofi
 cp ./rofidmenu.rasi /home/$USER/.config/rofi/rofidmenu.rasi
+cp ./config.rasi /home/$USER/.config/rofi/config.rasi
 [ -f /home/$USER/.config/i3/config ] && mv /home/$USER/.config/i3/config /home/$USER/.config/i3/config.original && cp ./i3config /home/$USER/.config/i3/config || echo "copia i3 config falló"
 [ -f /etc/i3status.conf ] && sudo mv /etc/i3status.conf /etc/i3status.conf.original && sudo cp ./i3status.conf /etc/i3status.conf || echo "copia i3status falló"
+[ -f /home/$USER/.config/i3/i3blocks.conf ] && mv /home/$USER/.config/i3/i3blocks.conf /home/$USER/.config/i3/i3blocks.conf.original && cp ./i3blocks.conf /home/$USER/.config/i3/i3blocks.conf|| echo "copia i3status falló"
