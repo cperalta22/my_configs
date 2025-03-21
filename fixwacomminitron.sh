5456f9eb2b4d23e937b1d209dc95549ecd22312f
cpa minitron <cpa@minitron>
2025-03-21 09:45:34 -0600

    fixwacom @ minitron

---
+++ fixwacomminitron.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+xsetwacom set "Wacom One by Wacom S Pen stylus" rotate half
+xsetwacom set "Wacom One by Wacom S Pen eraser" rotate half
+xsetwacom set "Wacom One by Wacom S Pen stylus"  maptooutput head-0
+xsetwacom set "Wacom One by Wacom S Pen eraser"  maptooutput head-0
