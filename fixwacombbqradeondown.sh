4d6d64311172e861a2c70478e246b4a3a3f4d6e8
cpa@bbq <cperalta@ifc.unam.mx>
2025-06-29 14:43:59 -0600

    radeon monitor changes, smart term to launch alacritty with different size fonts

--- fixwacombbqradeondown.sh
+++ fixwacombbqradeondown.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 xsetwacom set "Wacom One by Wacom M Pen stylus" rotate half
 xsetwacom set "Wacom One by Wacom M Pen eraser" rotate half
-xsetwacom set "Wacom One by Wacom M Pen stylus" maptooutput DP-2
-xsetwacom set "Wacom One by Wacom M Pen eraser" maptooutput DP-2
+xsetwacom set "Wacom One by Wacom M Pen stylus" maptooutput DisplayPort-1
+xsetwacom set "Wacom One by Wacom M Pen eraser" maptooutput DisplayPort-1
---
+++ smart_term_bbq.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Reemplaza 'eDP-1' con el nombre de tu monitor pequeño
+SMALL_MONITOR_NAME="DisplayPort-2"
+
+# Obtenemos el nombre del monitor que tiene el foco actual usando i3-msg y jq
+FOCUSED_MONITOR=$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused==true).output')
+
+# Si el monitor con foco es el pequeño, lanza el terminal con la config pequeña
+if [[ "$FOCUSED_MONITOR" == "$SMALL_MONITOR_NAME" ]]; then
+  alacritty --config-file ~/.alacritty_small.toml
+else
+  # De lo contrario, lanza la configuración normal
+  alacritty
+fi
